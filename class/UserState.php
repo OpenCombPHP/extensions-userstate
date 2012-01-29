67e0dd471fe24b28a9f0569dc2706a669ca7579c
alee <zq@alee2002.com>
2012-01-29 11:41:41 +0800

    create extension

---
+++ class/UserState.php
@@ -0,0 +1,15 @@
+<?php 
+namespace org\opencomb\userstate ;
+
+use org\opencomb\platform\ext\Extension ;
+
+class UserState extends Extension 
+{
+	/**
+	 * 载入扩展
+	 */
+	public function load()
+	{
+		// todo ...
+	}
+}
